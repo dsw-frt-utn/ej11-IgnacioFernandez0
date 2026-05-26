3d0f37607deaf44d4f9cc13180927ef343e3e8ae
Ignacio Fernandez <ignaciofernandez565@gmail.com>
2026-05-26 10:40:51 -0300

    CasoList Terminado

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +10,32 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> listaAlumnos = new();
+
+    public void AgregarAlumnos(Alumno alumno)
+    {
+        listaAlumnos.Add(alumno);
+    }
+
+    public List<Alumno> RetornarListaAlumnos()
+    {
+        return listaAlumnos;
+    }
+
+    public Alumno? BuscarAlumno(string nombre)
+    {
+        Alumno? alumnoAux = listaAlumnos.Find(x => x.Nombre.Equals(nombre));
+        return alumnoAux;
+    }
+
+    public void EliminarAlumno(Alumno alumno)
+    {
+        listaAlumnos.Remove(alumno);
+    }
+
+    public void EliminarAlumno(int indice)
+    {
+        listaAlumnos.RemoveAt(indice);
+    }
+
 }
